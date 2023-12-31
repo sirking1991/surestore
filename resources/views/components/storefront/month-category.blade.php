f09f51fa9a710324533a190db94dec77a68f55df
sirking1991 <sirking1991@gmail.com>
2023-12-31 11:13:26 +0800

    fixed category links in home page

--- resources/views/components/storefront/month-category.blade.php
+++ resources/views/components/storefront/month-category.blade.php
@@ -9,12 +9,13 @@
         </div>
     </div>
     <div class="row">
-        @foreach ($categories as $cat)
+        @foreach ($categories as $category)
         <div class="col-12 col-md-4 p-5 mt-3">
-            <a href="#"><img src="{{$cat->image_url}}"
-                    class="rounded-circle img-fluid border"></a>
-            <h5 class="text-center mt-3 mb-3">{{$cat->name}}</h5>
-            <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
+            <img src="{{$category->image_url}}" class="rounded-circle img-fluid border">
+            <h5 class="text-center mt-3 mb-3">{{$category->name}}</h5>
+            <p class="text-center">
+                <a href="{{ url('shop'.'?category-slug=' . $category->slug) }}" class="btn btn-success">Go Shop</a>
+            </p>
         </div>            
         @endforeach
     </div>
